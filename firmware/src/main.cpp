1bf29989eb74b58402cb01bd5be0925fd717408f
RoryHemmings <roryhemmings@ucla.edu>
2023-11-19 21:36:17 -0800

    Attempt to add reconnect feature

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,20 +11,29 @@
 
 #define LED_PIN LED_BUILTIN
 
-#define SERVER_ADDRESS ""
+#define SERVER_ADDRESS "172.20.10.2"
 #define SERVER_PORT 3001
 
 /* Constants */
 const char SSID[] = SECRET_SSID;
 const char PASS[] = SECRET_PASS;
 const int DEBUG_INTERVAL = 5000; /* Interval at which to update board information (milliseconds) */
-static const SOLENOID_PINS[] = { 11, 12 };
+static const int SOLENOID_PINS[] = {11, 12};
 
 /* Statics */
-int status = WL_IDLE_STATUS;            /* Radio Status */
-int led_state = LOW;                    /* LED State */
+int status = WL_IDLE_STATUS; /* Radio Status */
+int led_state = LOW;         /* LED State */
 
 WiFiClient client;
+
+void connect_to_server()
+{
+    if (client.connect(SERVER_ADDRESS, SERVER_PORT)) {
+        Serial.println("connected to server");
+
+        client.println("Hello From Arduino");
+    }
+}
 
 void setup()
 {
@@ -48,54 +57,36 @@ void setup()
     Serial.println("Connection Successful!");
     Serial.println("----------------------");
 
-	if (client.connect(SERVER_ADDRESS, SERVER_PORT)) {
-		Serial.println("connected to server");
-
-		Client.println("Hello From Arduino");
-	}
+    connect_to_server();
 }
 
 void loop()
 {
 
-	// TODO get state machine working for connection led
-	// if valve is closed and we recieve a request to water, execute that request, and queue other requests
-	// continuously maintain socket connection between arduino and api
-	// wait for data and respond accordingly
-	// monitor stuff and send it to arduino when nessecary
+    // TODO get state machine working for connection led
+    // if valve is closed and we recieve a request to water, execute that request, and queue other requests
+    // continuously maintain socket connection between arduino and api
+    // wait for data and respond accordingly
+    // monitor stuff and send it to arduino when nessecary
 
-	// Handle Connection
-	// switch (client.status()) {
-	// case /* Connected */:
-	// 	// read if anything available
-	// 	break;
-	// case /* Disconnected */:
-	// 	// attempt to reconnect
-	// 	while (!client.connect(server, 80))
-	// 		;
-	// 	break;
-	// }
+    while (client.available()) {
+        char c = client.read();
+        // switch (c) {
+        // case 'O':
+        // 	digitalWrite(LED_PIN, HIGH);
+        // 	break;
+        // case 'C':
+        // 	digitalWrite(LED_PIN, LOW);
+        // 	break;
+        // }
 
-	while (client.available()) {
-		char c = client.read();
-		// switch (c) {
-		// case 'O':
-		// 	digitalWrite(LED_PIN, HIGH);
-		// 	break;
-		// case 'C':
-		// 	digitalWrite(LED_PIN, LOW);
-		// 	break;
-		// }
+        Serial.write(c);
+    }
 
-		Serial.write(c);
-	}
+    if (!client.available()) {
+        Serial.println();
+        Serial.println("disconnecting from server");
 
-	if (!client.available()) {
-		Serial.println();
-		Serial.println("disconnecting from server");
-
-		client.stop();
-
-		while (true);
-	}
+        client.stop();
+    }
 }
