306ca0d08c9b57e98b665d0634599d1ef53b74c8
kalcow <81459450+kalcow@users.noreply.github.com>
2023-12-03 20:53:14 -0800

    Added Plant data for specific plants

--- app/plant-tracker/App.tsx
+++ app/plant-tracker/App.tsx
@@ -1,23 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from "react-native";
+import { LineChart } from 'react-native-chart-kit';
+import { LineChartData } from 'react-native-chart-kit/dist/line-chart/LineChart';
 
 export default function App() {
-  const [data, setData] = useState([])
-  const getData = async  () => {
-    console.log('hello'); 
+  const [data, setData] = useState([]);
+  const [chartData, setChartData] = useState<LineChartData>();
+  const [showGraph, setShowGraph] = useState(false);
+  const getData = async () => {
     const r = await fetch('http://172.20.10.3:3000/all')
-    const _data = await r.json(); 
-    setData(_data);  
+    const _data = await r.json();
+    setData(_data);
+  }
+
+  const screenWidth = Dimensions.get("window").width;
+
+  function generateData(data: any[]) {
+    // console.log(data); 
+    if (data.length !== 0) {
+      let _data: LineChartData  = {
+        labels: data.map((d, i) => { return i.toString(); }),
+        datasets: [
+          {
+            data: data.map((d) => { return d.reading })
+          }
+        ]
+      }; 
+      setChartData(_data); 
+      setShowGraph(true); 
+    }
   }
 
   useEffect(() => {
-    console.log(data); 
-  }, [data])
+    generateData(data);
+  }, [data]);
+
+
   return (
     <View style={styles.container}>
       <Text onPress={getData}>Open up App.ts to start working on your app!</Text>
       <StatusBar style="auto" />
+      {showGraph && <LineChart
+        data={chartData}
+        width={screenWidth}
+        height={256}
+        verticalLabelRotation={30}
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+      />}
     </View>
   );
 }
