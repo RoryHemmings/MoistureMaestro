62afc5f12f8490f096d8da05d4b12a8b9cde5b08
kalcow <81459450+kalcow@users.noreply.github.com>
2023-12-05 22:43:33 -0800

    desc to asc

--- app/plant-tracker/App.tsx
+++ app/plant-tracker/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
   }
 
   return (
-    <AppContext.Provider value={{ ip: "172.16.0.47" }}>
+    <AppContext.Provider value={{ ip: "172.16.0.4" }}>
       <NavigationContainer>
         <StatusBar style="auto" />
         <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#FFFFFF' } }} >
